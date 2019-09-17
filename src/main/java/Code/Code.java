1dd864069d7e7e4ed987b77964ce11fa6f628df5
pedago <pedago@pcalb-mm0622.univ-jfc.fr>
2019-09-17 15:56:08 +0200

    version 1.0

---
+++ src/main/java/Code/Code.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Code;
+
+/**
+ *
+ * @author pedago
+ */
+public class Code {
+    
+    /**
+     * Ajoute un à un. Puis renvoie un.
+     * @return resultat équivalent à un
+     */
+    public int ajoute(){
+        int z; // resultat de l'addition
+        int x =1; // les deux variables additionnées
+        int y =1; // les deux variables additionnées
+        
+         z = x + y;
+        
+        return 2;
+    }
+    
+}
